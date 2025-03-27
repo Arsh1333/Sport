8f0cdf4f971222f06e1e520816ffd38bab84965f
Arsh <pawar.arsh1333@gmail.com>
2025-03-27 14:11:53 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // res.send("Hello World");
+  res.send("Hello");
 });
 
 app.listen(process.env.PORT, () => {
